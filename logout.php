6ea51fed6a4631c193466ff4b87828c65273ea9f
15god <sirenko.stepan1@mail.ru>
2023-01-10 20:48:18 +0300

    final routing done

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_destroy();
-setcookie('userKey', '', time()-3600, '/');
-header('Location:' . $_SERVER['HTTP_REFERER']);
-exit;
-/*
- * $_SESSION['last_page'] = $_SERVER['PHP_SELF'];
- */
